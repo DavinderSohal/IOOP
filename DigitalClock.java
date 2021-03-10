41a677db01ae3cfbee978a15d0f4192892569561
DavinderSohal <sohal00026@gmail.com>
2021-03-10 14:43:46 +0530

    Minor changes in digital clock

--- DigitalClock.java
+++ DigitalClock.java
@@ -39,6 +39,7 @@ public class DigitalClock{
             try{
                 Thread.sleep(1000);
             }catch(Exception e){
+                e.printStackTrace();
             }
         }
     }
--- DigitalClock2.java
+++ DigitalClock2.java
@@ -7,6 +7,7 @@ public class DigitalClock2{
                     try{
                         Thread.sleep(1000);
                     }catch(Exception e){
+                        e.printStackTrace();
                     }
                 }
             }
