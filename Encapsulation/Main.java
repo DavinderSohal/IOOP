6a86e2b2851adcb54d8f3b18d2a56ca0b5dc7979
DavinderSohal <sohal00026@gmail.com>
2021-04-08 17:37:22 +0530

    changes in encapsulation

--- Encapsulation/Main.java
+++ Encapsulation/Main.java
@@ -2,12 +2,27 @@ package Encapsulation;
 
 public class Main{
     public static void main(String[] args){
-        User usr = new User();
+        User usr1 = new User();
+        User usr2 = new User();
+        User usr3 = new User();
 
-        usr.setFirst_name("Davinder");
-        usr.setLast_name("Sohal");
+        usr1.setFirst_name("Davinder");
+        usr1.setLast_name("Sohal");
+        usr1.setId(555555);
+        usr1.checkPassword();
 
-        System.out.println("\nIn uppercase: " + usr.getFirst_name().toUpperCase() + " " + usr.getLast_name().toUpperCase());
-        System.out.println("In lowercase: " + usr.getFirst_name().toLowerCase() + " " + usr.getLast_name().toLowerCase());
+        usr2.setFirst_name("dddd");
+        usr2.setLast_name("ssss");
+        usr2.setId(444455);
+        usr2.checkPassword();
+
+        usr3.setFirst_name("davinder");
+        usr3.setLast_name("singh");
+        usr3.setId(1234);
+        usr3.checkPassword();
+
+        System.out.println("\nIn uppercase: " + usr1.getFirst_name()+ " " + usr1.getLast_name() + "\nID: " + usr1.getId());
+//        System.out.println("In lowercase: " + usr1.getFirst_name().toLowerCase() + " " + usr1.getLast_name().toLowerCase());
+        System.out.println("First name: " + usr2.getFirst_name() + "\nLast name: " + usr2.getLast_name() + "\nID: " + usr2.getId());
     }
 }
--- Encapsulation/User.java
+++ Encapsulation/User.java
@@ -3,9 +3,21 @@ package Encapsulation;
 public class User{
     private String first_name;
     private String last_name;
+    private Integer id;
+
+    public Integer getId(){
+        return id;
+    }
+
+    public void setId(Integer id){
+//        if(id > 2000){
+            this.id = id;
+//        }
+    }
 
     public String getFirst_name(){
-        return first_name;
+//        return first_name;
+        return first_name.toUpperCase();
     }
 
     public void setFirst_name(String first_name){
@@ -13,10 +25,21 @@ public class User{
     }
 
     public String getLast_name(){
-        return last_name;
+//        return last_name;
+        return last_name.toUpperCase();
     }
 
     public void setLast_name(String last_name){
         this.last_name = last_name;
+    }
+
+    public void checkPassword(){
+        int password = 1234;
+        if(id == password){
+            System.out.printf("\n%s %s, System is open\n", first_name, last_name);
+        }else{
+            System.out.printf("\n%s %s, System is closed\n", first_name, last_name);
+        }
+
     }
 }
