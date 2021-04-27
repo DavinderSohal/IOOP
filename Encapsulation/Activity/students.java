5a07af683278b6f8f93eadde7eb5b0c018222741
DavinderSohal <sohal00026@gmail.com>
2021-04-27 20:35:31 +0530

    minor changes

--- Encapsulation/Activity/students.java
+++ Encapsulation/Activity/students.java
@@ -2,18 +2,28 @@ package Encapsulation.Activity;
 
 
 public class students extends person{
-    int GPA;
+    double GPA;
+    String specialization;
 
-    public students(String first_name, String last_name, String password, int GPA){
+    public students(String first_name, String last_name, String password, double GPA, String specialization){
         super(first_name, last_name, password);
         this.GPA = GPA;
+        this.specialization = specialization;
     }
 
-    public int getGPA(){
+    public double getGPA(){
         return GPA;
     }
 
-    public void setGPA(int GPA){
+    public void setGPA(double GPA){
         this.GPA = GPA;
+    }
+
+    public String getSpecialization(){
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization){
+        this.specialization = specialization;
     }
 }
--- Encapsulation/Activity/teacher.java
+++ Encapsulation/Activity/teacher.java
@@ -2,7 +2,15 @@ package Encapsulation.Activity;
 
 
 public class teacher extends person{
+
     String date_of_hiring;
+    int id_number;
+
+    public teacher(String first_name, String last_name, String password, String date_of_hiring, int id_number){
+        super(first_name, last_name, password);
+        this.date_of_hiring = date_of_hiring;
+        this.id_number = id_number;
+    }
 
     public String getDate_of_hiring(){
         return date_of_hiring;
@@ -12,9 +20,12 @@ public class teacher extends person{
         this.date_of_hiring = date_of_hiring;
     }
 
-    public teacher(String first_name, String last_name, String password, String date_of_hiring){
-        super(first_name, last_name, password);
-        this.date_of_hiring = date_of_hiring;
+    public int getId_number(){
+        return id_number;
+    }
+
+    public void setId_number(int id_number){
+        this.id_number = id_number;
     }
 }
 
